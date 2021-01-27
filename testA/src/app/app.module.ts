7050410f16f98393e53a354885bf7a28b20a28ff
vinodb2806 <Vinod.bhawsar2806@gmail.com>
2021-01-27 14:16:46 +0530

    new component

--- testA/src/app/app.module.ts
+++ testA/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
